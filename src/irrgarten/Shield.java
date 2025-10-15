a22aa35a1988f49e88dad3b79d706058a377a60f
carmen <carmen@carmen-Prestige-14-A12UC>
2025-10-15 16:55:15 +0200

    cositas

--- src/irrgarten/Shield.java
+++ src/irrgarten/Shield.java
@@ -5,11 +5,21 @@ public class Shield {
     private float protection;
     private int uses;
     
+
+    // This is a constructor for the `Shield` class in Java. It takes two parameters, `protection` of type
+    // `float` and `uses` of type `int`, and initializes the `protection` and `uses` instance variables of
+    // the `Shield` object with the values passed as arguments to the constructor.
     public Shield (float protection, int uses) {
         this.protection = protection; 
         this.uses = uses;
     }
     
+    /**
+     * @return The method `protect` is returning a float value. The value being returned is either the
+     * `protection` value if `uses` is greater than 0, or the protection if `uses` is not greater than 0.
+     * Additionally, the `uses` variable is decremented by 1 after the `protection` value is assigned to
+     * `salida`.
+     */    
     public float protect (){
         float salida = 0.0f;
         
@@ -21,10 +31,20 @@ public class Shield {
         return salida;
     }
     
+    /**
+
+     * @return The `discard()` method is returning a boolean value, which is the result of calling the
+     * `discardElement()` method from the `Dice` class with the `uses` parameter.
+     */
     public boolean discard(){
         return Dice.discardElement(uses);
     }
         
+
+    /**
+     * @return The `toString` method is returning a string representation of an object of type `S`, which
+     * includes the values of the `protection` and `uses` variables enclosed in square brackets.
+     */
     @Override
     public String toString () {
         return "S[" + protection + "," + uses + "]";
