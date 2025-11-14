2a0055bee623df075a2bb9792a529396df838821
elenaroderojimenez <elenaroderojim@gmail.com>
2025-11-14 18:45:19 +0100

    Primer codigo de la practica 4 (no compila)

--- src/irrgarten/FuzzyPlayer.java
+++ src/irrgarten/FuzzyPlayer.java
@@ -1,6 +1,29 @@
 package irrgarten;
 
-public class FuzzyPlayer {
-
+public class FuzzyPlayer extends Player{
     
+    public FuzzyPlayer(Player other){
+        
+    }
+    
+    public Directions move(Directions directiion, Directions[] validMoves){
+        
+    }
+    
+    @Override
+    public float attack(){
+        return sumWeapons() + Dice.intensity(strength); //Getter de strength??
+    }
+    
+    //@Override???
+    protected float defensiveEnergy(){
+        return sumShields() + Dice.intensity(intelligence); //Getter de intelligence??
+    }
+    
+    @Override
+    public String toString(){
+        String s = "Fuzzy "; 
+        s += super.toString();//????
+        return s; 
+    }
 }
